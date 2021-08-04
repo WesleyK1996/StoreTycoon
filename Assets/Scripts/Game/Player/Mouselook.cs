a3aa132abf20ecc576fe8d7c5321d0dc4419f90b
WesleyK1996 <zeriotosmoke@gmail.com>
2021-08-04 16:54:55 +0200

    1

---
+++ Assets/Scripts/Game/Player/Mouselook.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouselook : MonoBehaviour
+{
+    float sensitivity;
+    float xRot;
+    float yRot;
+    Transform playerBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sensitivity = 150F;//replace with playerprefs
+        xRot = -90;
+        yRot = 0;
+        playerBody = transform.parent;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        xRot += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        yRot += Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        yRot = ClampAngle(yRot, -90, 90);
+        transform.localRotation = Quaternion.AngleAxis(yRot, -Vector3.right);
+        playerBody.rotation = Quaternion.AngleAxis(xRot, Vector3.up);
+    }
+
+    public float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+            angle += 360F;
+        if (angle > 360F)
+            angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
---
+++ Assets/Scripts/Game/Player/Move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(transform.forward * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(-transform.right * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(-transform.forward * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(transform.right * Time.deltaTime);
+    }
+}
