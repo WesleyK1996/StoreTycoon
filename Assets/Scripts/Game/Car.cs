9df64c3d622ba893ec77d492eae082c4c3604bae
WesleyK1996 <zeriotosmoke@gmail.com>
2021-08-16 16:47:10 +0200

    car goes to parking space

--- Assets/Scripts/Game/Car.cs
+++ Assets/Scripts/Game/Car.cs
@@ -5,93 +5,205 @@ using UnityEngine.AI;
 
 public class Car : MonoBehaviour
 {
-    [Tooltip("First 2 values need to be the front wheels. otherwise the car won't steer correctly.")]
     public Transform[] wheels;
     public Rigidbody rb;
     public NavMeshAgent agent;
 
-    bool parked;
-    bool coming = true;
-    string road;
+    public string target;
+    Vector3 targetPos;
+    bool driving;
+    bool inLot;
 
-    //-2.49272
-    //17.12534
+    public ParkingSpace parkingSpace;
+
+
+    public delegate Vector3 DestinationMethod();
+    DestinationMethod GetDestination;
+
+    public enum CarStatus
+    {
+        coming,
+        parking,
+        parked,
+        going
+    }
+    CarStatus status = CarStatus.coming;
 
     private void Start()
     {
-        road = Random.Range(0, 2) == 0 ? "Top1" : "Bottom1";
-        if (road == "Top1")
+        target = Random.Range(0, 2) == 0 ? "Top1" : "Bottom1";
+        if (target == "Top1")
             transform.eulerAngles += new Vector3(0, 180, 0);
-        transform.position = GetRoadCenterVector(road);
+        transform.position = GetRoadCenterVector();
 
         agent.enabled = true;
-        agent.SetDestination(GetDestination());
     }
 
     void Update()
     {
-        if (!parked)
-            Drive();
+        if (!driving && status != CarStatus.parked)
+            StartCoroutine(Drive());
     }
 
-    void Drive()
+    IEnumerator Drive()
     {
-        foreach (Transform wheel in wheels)
-            wheel.Rotate(-Vector3.forward * rb.velocity.z);
-        if (agent.remainingDistance < .5f && !parked)
-            agent.SetDestination(GetDestination());
+        driving = true;
+
+        SetBehaviour();
+        targetPos = GetDestination();
+        agent.SetDestination(targetPos);
+
+        while (Vector3.Distance(transform.position, targetPos) > .1f)
+            yield return new WaitForEndOfFrame();
+        driving = false;
     }
 
-    private Vector3 GetDestination()
+    private void SetBehaviour()
     {
-        if (coming)
+        switch (status)
         {
-            switch (road)
-            {
-                case "Top1":
-                    road = "Top4";
-                    break;
-                case "Bottom1":
-                    road = "Bottom5";
-                    break;
-                case "Top4":
-                case "Bottom5":
-                    road = "Top5";
-                    break;
-                case "Top5":
-                    road = "In";
-                    break;
-                case "In":
-                    return GetParkingSpace();
-                    break;
-            }
+            case CarStatus.coming:
+                GetDestination = GetDestinationComing;
+                break;
+            case CarStatus.parking:
+                GetDestination = GetDestinationParking;
+                break;
+            case CarStatus.going:
+                GetDestination = GetDestinationGoing;
+                break;
         }
-        else
-        {
-
-        }
-        print(road);
-        return GetRoadCenterVector(road);
     }
 
-    private Vector3 GetParkingSpace()
+    Vector3 GetDestinationComing()
+    {
+        switch (target)
+        {
+            case "Top1":
+                target = "Top4";
+                break;
+            case "Bottom1":
+                target = "Bottom5";
+                break;
+            case "Top4":
+            case "Bottom5":
+                if (CanPark(out parkingSpace))
+                {
+                    status = CarStatus.parking;
+                    target = "Top5";
+                }
+                else
+                    target = target == "Top4" ? "Top10" : "Bottom10";
+                break;
+        }
+        return GetRoadCenterVector();
+    }
+
+    Vector3 GetDestinationParking()
+    {
+        switch (target)
+        {
+            case "Top5":
+                target = "In";
+                return GetRoadCenterVector();
+            default:
+                print(ParkingLot.Instance.GetDestination(this));
+                return ParkingLot.Instance.GetDestination(this);
+        }
+    }
+
+    Vector3 GetDestinationGoing()
     {
         throw new System.NotImplementedException();
     }
 
-    Vector3 GetRoadCenterVector(string road)
-    {
-        Vector3 r = Vector3.zero;
+    //void Drive()
+    //{
+    //    foreach (Transform wheel in wheels)
+    //        wheel.Rotate(-Vector3.forward * rb.velocity.z);
 
-        foreach (Transform t in GameManager.Instance.road)
-            if (t.name == road)
+    //    switch(status)
+    //    {
+    //        case CarStatus.coming:
+    //            break;
+    //    }
+    //    if (Vector3.Distance(transform.position, targetPos) > .1f)
+    //        agent.SetDestination(GetDestination());
+    //}
+
+
+
+    //private Vector3 GetDestination()
+    //{
+    //    switch (status)
+    //    {
+    //        case CarStatus.coming:
+    //            switch (target)
+    //            {
+    //                case "Top1":
+    //                    target = "Top4";
+    //                    break;
+    //                case "Bottom1":
+    //                    target = "Bottom5";
+    //                    break;
+    //                case "Top4":
+    //                case "Bottom5":
+    //                    if (CanPark(out parkingSpace))
+    //                    {
+    //                        status = CarStatus.parking;
+    //                        target = "Top5";
+    //                    }
+    //                    else
+    //                        target = target == "Top4" ? "Top10" : "Bottom10";
+    //                    break;
+    //            }
+    //            break;
+    //        case CarStatus.parking:
+    //            switch (target)
+    //            {
+    //                case "Top5":
+    //                    target = "In";
+    //                    break;
+    //                case "In":
+    //                    return ParkingLot.Instance.GetDestination(this);
+    //            }
+    //            break;
+    //    }
+    //    Debug.Break();
+    //    return GetRoadCenterVector();
+    //}
+
+    bool CanPark(out ParkingSpace parkingSpace)
+    {
+        foreach (ParkingSpace space in FindObjectOfType<ParkingLot>().spaces)
+            if (space.free)
             {
-                r = t.GetComponent<Collider>().bounds.center;
-                if (road.Contains("Bottom"))
-                    r.z -= .4f;
-                r.y = 1;
-                return r;
+                parkingSpace = space;
+                space.free = false;
+                return true;
             }
+        parkingSpace = null;
+        return false;
+    }
+
+    Vector3 GetRoadCenterVector()
+    {
+        foreach (Transform t in GameManager.Instance.road)
+            if (t.name == target)
+                return GetRoadCenterVector(t.gameObject);
+        return Vector3.zero;
+    }
+
+    public static Vector3 GetRoadCenterVector(GameObject road)
+    {
+        Vector3 r = road.GetComponent<Collider>().bounds.center;
+        r.y += 1;
+        if (road.name.StartsWith("Bottom")) r.z -= .4f;
+
         return r;
+    }
+
+    public void SetStatus(CarStatus status)
+    {
+        this.status = status;
     }
 }
--- Assets/Scripts/Game/ParkingLot.cs
+++ Assets/Scripts/Game/ParkingLot.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,10 +6,56 @@ using UnityEngine;
 public class ParkingSpace
 {
     public bool free;
-    public GameObject space;
+    public GameObject gameObject;
 }
 
 public class ParkingLot : MonoBehaviour
 {
+    public static ParkingLot Instance;
     public List<ParkingSpace> spaces = new List<ParkingSpace>();
+
+
+    public delegate Vector3 DestinationMethod(Car car);
+    public DestinationMethod GetDestination;
+    Car car;
+
+    void Start()
+    {
+        if (Instance == null)
+            Instance = this;
+        else Destroy(gameObject);
+
+        switch (name)
+        {
+            case "Parking1":
+                GetDestination = Parking1;
+                break;
+        }
+    }
+
+    Vector3 GetRoadCenterVector(string name, bool road)
+    {
+        foreach (Transform t in road ? transform.Find("Road") : transform.Find("Spaces"))
+            if (t.name == name)
+                return Car.GetRoadCenterVector(t.gameObject);
+        return Vector3.zero;
+    }
+
+    Vector3 Parking1(Car car)//parking for lot 1
+    {
+        bool road = true;
+        switch (car.target)
+        {
+            case "A2":
+                car.target = car.parkingSpace.gameObject.name;
+                road = false;
+                car.SetStatus(Car.CarStatus.parked);
+                break;
+            case "In":
+                car.target = "A2";
+                break;
+        }
+
+        return GetRoadCenterVector(car.target, road);
+    }
 }
